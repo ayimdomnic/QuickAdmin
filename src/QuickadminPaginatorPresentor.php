5c69cc7ae6b686611174e57badb1cbf8505c1c78
Odhiambo Dormnic <ayimdomnic@outlook.com>
2016-03-26 16:35:44 -0400

    Applied fixes from StyleCI

--- src/QuickadminPaginatorPresentor.php
+++ src/QuickadminPaginatorPresentor.php
@@ -3,22 +3,21 @@
 use Illuminate\Contracts\Pagination\Presenter;
 use Illuminate\Pagination\BootstrapThreePresenter;
 
-
 class QuickadminPaginatorPresenter extends BootstrapThreePresenter implements Presenter
 {
-	public function render()
-	{
-		if($this->hasPages()){
-			return sprintf(
-			//list them here
-				'<ul class="pagination pagination-sm no-margin">%s %s %s</ul>',
-				$this->getPrevioursButton(),
-				$this->getLinks(),
-				$this->getNextButton()
+    public function render()
+    {
+        if ($this->hasPages()) {
+            return sprintf(
+            //list them here
+                '<ul class="pagination pagination-sm no-margin">%s %s %s</ul>',
+                $this->getPrevioursButton(),
+                $this->getLinks(),
+                $this->getNextButton()
 
-			);
-			//return something maybe
-			return '';
-		}
-	}
+            );
+            //return something maybe
+            return '';
+        }
+    }
 }
--- src/QuickadminTask.php
+++ src/QuickadminTask.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class QuickadminTask extends Model
 {
-	public function scopeActive($query, $active =true)
-	{
-		return $query->whereActive($active);
-	}
+    public function scopeActive($query, $active = true)
+    {
+        return $query->whereActive($active);
+    }
 }
